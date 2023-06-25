57568b180e1afa6f55c9aff8ae138214cf429c7e
xac <jiangfengxi.c@gmail.com>
2023-06-26 00:45:00 +0800

    mods setup

--- .klei/DoNotStarveTogether/DoNotStarveTogether/Cluster_1/mods/dedicated_server_mods_setup.lua
+++ .klei/DoNotStarveTogether/DoNotStarveTogether/Cluster_1/mods/dedicated_server_mods_setup.lua
@@ -10,17 +10,20 @@
 	--Example: http://steamcommunity.com/sharedfiles/filedetails/?id=379114180
 	--ServerModCollectionSetup("379114180")
 
-ServerModSetup("666155465")
-ServerModSetup("1185229307")
-ServerModSetup("1207269058")
-ServerModSetup("1221281706")
-ServerModSetup("347079953")
-ServerModSetup("375850593")
-ServerModSetup("375859599")
-ServerModSetup("378160973")
-ServerModSetup("569043634")
-ServerModSetup("623749604")
-ServerModSetup("661253977")
-ServerModSetup("666155465")
-ServerModSetup("786556008")
-ServerModSetup("856487758")
+ServerModSetup("347079953") --Display Food Values
+ServerModSetup("362175979") --Wormhole Marks [DST]
+ServerModSetup("375850593") --Extra Equip Slots
+ServerModSetup("375859599") --Health Info
+ServerModSetup("378160973") --Global Positions
+ServerModSetup("569043634") --Campfire Respawn
+ServerModSetup("623749604") --[DST] Storeroom
+ServerModSetup("661253977") --Don't Drop Everything
+ServerModSetup("666155465") --SHow me
+ServerModSetup("812723897") --Extended Map Icons
+ServerModSetup("856487758") --Quick Drop
+ServerModSetup("1185229307") --史诗般的血量条 (Epic Healthbar)
+ServerModSetup("1207269058") --简易血条DST
+ServerModSetup("1221281706") --The Forge Items Pack
+ServerModSetup("2078243581") --Display Attack Range
+ServerModSetup("2477889104") --Beefalo Status Bar
+
